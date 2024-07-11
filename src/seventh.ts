00a267cfcd27d67dc00720a64062dbdfb1927f01
Musfirah Murtaza Korai <161106882+musfirahkorai@users.noreply.github.com>
2024-07-11 12:14:37 +0500

    Add files via upload

---
+++ src/seventh.ts
@@ -0,0 +1,38 @@
+// Function
+
+// Function Invocation means calling Function
+
+// To execute a function, you simply invoke it by using its name
+// followed by parentheses.
+
+// Function of a company that recently hired
+// new people and welcoming them
+
+//Function Dfine
+
+function greet(name:string, id:number){
+     console.log(`Welcome, ${name} and your id is ${id}`);
+     
+}
+
+//Function Invocation (Call)
+greet("musfira",1);
+
+
+function muusfira(name:string, id:number) {
+     console.log(`Welcome, ${name} and your id is ${id}`);
+
+}
+muusfira("amna",2)
+
+
+function calculateAverage(numbers: number[]): number {
+     return numbers.reduce((acc, num) => acc + num, 0) / numbers.length;
+}
+
+// New Function
+
+function calculateAveragew(number: number[]): number {
+     return number.reduce((acc, num) => acc + num, 0) / SVGAnimatedNumberList.length;
+}
+
